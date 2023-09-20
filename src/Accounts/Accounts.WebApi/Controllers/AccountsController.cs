7f4f3263cba8e23cef4c351f327d0c6c30beb3e0
Kristoffer LaRue <kristoffer.larue@globalpay.com>
2023-09-19 21:06:13 -0600

    checking

---
+++ src/Accounts/Accounts.WebApi/Controllers/AccountsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Accounts.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        public AccountsController() 
+        {
+            
+        }
+    }
+}
